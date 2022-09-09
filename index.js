ed165e16d7cc043f2589956b30344b72899b7aca
projectejiri <110473227+projectejiri@users.noreply.github.com>
2022-09-09 07:17:04 +0100

    added files

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const Datastore = require("nedb");
+
+const app = express();
+app.listen(3000, () => console.log("listening at 3000"));
+app.use(express.static("public"));
+app.use(express.json({ limit: "1mb" }));
+
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+app.post("/api", (request, response) => {
+  console.log("New Entry");
+  console.log(request.body);
+  const data = request.body;
+  response.json({
+    status: "success",
+
+  });
+});
